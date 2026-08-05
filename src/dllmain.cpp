619de0a5e041674d8a03815319b83a7950eb0061
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 10:38:22 -0400

    Add DLL entry point

---
+++ src/dllmain.cpp
@@ -0,0 +1,31 @@
+#include "reapercore/application.hpp"
+
+#include <Windows.h>
+
+namespace
+{
+    DWORD WINAPI bootstrap(const LPVOID parameter)
+    {
+        const auto module = static_cast<HMODULE>(parameter);
+        reapercore::application app(module);
+        return app.run();
+    }
+}
+
+BOOL APIENTRY DllMain(const HMODULE module, const DWORD reason, LPVOID)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(module);
+
+        const HANDLE thread = CreateThread(nullptr, 0, bootstrap, module, 0, nullptr);
+        if (!thread)
+        {
+            return FALSE;
+        }
+
+        CloseHandle(thread);
+    }
+
+    return TRUE;
+}

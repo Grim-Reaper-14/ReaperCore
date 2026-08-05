997425d0eccca7456c837d99af2df9a4faecbda2
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 10:43:59 -0400

    Use Core for application startup and shutdown

--- src/application.cpp
+++ src/application.cpp
@@ -1,8 +1,5 @@
 #include "reapercore/application.hpp"
 
-#include <chrono>
-#include <thread>
-
 namespace reapercore
 {
     application::application(const HMODULE module) noexcept :
@@ -12,32 +9,16 @@ namespace reapercore
 
     DWORD application::run()
     {
-        if (!m_logger.initialize())
-        {
-            return ERROR_OPEN_FAILED;
-        }
+        if (!m_core.initialize())
+            return ERROR_INITIALIZATION_FAILED;
 
-        m_logger.write(log_level::info, "ReaperCore loaded successfully.");
-        m_logger.write(log_level::info, "Press END to unload cleanly.");
+        m_core.log().write(log_level::info, "ReaperCore loaded successfully.");
+        m_core.log().write(log_level::info, "Press END to unload cleanly.");
 
-        while (m_running.load(std::memory_order_relaxed))
-        {
-            if ((GetAsyncKeyState(VK_END) & 1) != 0)
-            {
-                request_stop();
-            }
+        m_core.run();
+        m_core.shutdown();
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        }
-
-        m_logger.write(log_level::info, "ReaperCore shutting down.");
-        m_logger.shutdown();
         FreeLibraryAndExitThread(m_module, EXIT_SUCCESS);
         return EXIT_SUCCESS;
-    }
-
-    void application::request_stop() noexcept
-    {
-        m_running.store(false, std::memory_order_relaxed);
     }
 }

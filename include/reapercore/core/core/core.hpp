66a9f87b54b487f776dc1c44f3c51cf4372ac8d2
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 23:24:02 -0400

    Wire event manager into core ownership

--- include/reapercore/core/core/core.hpp
+++ include/reapercore/core/core/core.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "reapercore/backend/backend.hpp"
+#include "reapercore/core/events/event_manager.hpp"
 #include "reapercore/core/file_system/file_system_manager.hpp"
 #include "reapercore/core/folder_system/folder_system_manager.hpp"
 #include "reapercore/core/logging/logging_manager.hpp"
@@ -20,6 +21,7 @@ namespace reapercore
         [[nodiscard]] File_System_Manager& files() noexcept;
         [[nodiscard]] Settings_System_Manager& settings() noexcept;
         [[nodiscard]] Logging_Manager& logging() noexcept;
+        [[nodiscard]] Event_Manager& events() noexcept;
         [[nodiscard]] ReaperCore_Lua_System& lua() noexcept;
         [[nodiscard]] Backend& backend() noexcept;
 
@@ -28,6 +30,7 @@ namespace reapercore
         File_System_Manager m_files;
         Settings_System_Manager m_settings;
         Logging_Manager m_logging;
+        Event_Manager m_events;
         ReaperCore_Lua_System m_lua;
         Backend m_backend;
         bool m_initialized{};

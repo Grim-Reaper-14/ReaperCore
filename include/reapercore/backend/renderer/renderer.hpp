bbaefa659e87ef85f375a4ce976dabb867356977
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-06 04:41:34 -0400

    Repair renderer interface

--- include/reapercore/backend/renderer/renderer.hpp
+++ include/reapercore/backend/renderer/renderer.hpp
@@ -18,4 +18,17 @@ namespace reapercore
     {
     public:
         bool initialize(Logging_Manager& logging, D3D12_Backend& d3d12) noexcept;
-        void render_frame() noexcept
+        void render_frame() noexcept;
+        void shutdown() noexcept;
+
+        [[nodiscard]] bool ready() const noexcept;
+        [[nodiscard]] Render_Statistics statistics() const noexcept;
+
+    private:
+        Logging_Manager* m_logging{};
+        D3D12_Backend* m_d3d12{};
+        std::atomic_bool m_ready{false};
+        std::atomic_uint64_t m_frame_count{0};
+        std::atomic_uint64_t m_skipped_frames{0};
+    };
+}

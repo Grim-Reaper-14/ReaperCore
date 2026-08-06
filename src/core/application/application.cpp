13f201f3be33712a4dec8c9609f08c8aec237d1e
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-06 04:49:35 -0400

    Remove unreachable application return

--- src/core/application/application.cpp
+++ src/core/application/application.cpp
@@ -10,9 +10,7 @@ namespace reapercore
     DWORD Application::run()
     {
         if (!m_core.initialize())
-        {
             return ERROR_DLL_INIT_FAILED;
-        }
 
         m_core.logging().info("application", "ReaperCore loaded successfully.");
         m_core.logging().info("application", "Press END to unload cleanly.");
@@ -21,6 +19,5 @@ namespace reapercore
         m_core.shutdown();
 
         FreeLibraryAndExitThread(m_module, EXIT_SUCCESS);
-        return EXIT_SUCCESS;
     }
 }

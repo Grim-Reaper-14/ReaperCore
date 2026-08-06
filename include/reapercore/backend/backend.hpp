b901a367e915825dcf9628cfcc34e75124d07dff
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-06 01:04:47 -0400

    Wire event manager into backend interface

--- include/reapercore/backend/backend.hpp
+++ include/reapercore/backend/backend.hpp
@@ -6,9 +6,11 @@
 
 #include <atomic>
 #include <chrono>
+#include <cstdint>
 
 namespace reapercore
 {
+    class Event_Manager;
     class Logging_Manager;
     class ReaperCore_Lua_System;
     class Settings_System_Manager;
@@ -19,7 +21,8 @@ namespace reapercore
         bool initialize(
             Logging_Manager& logging,
             Settings_System_Manager& settings,
-            ReaperCore_Lua_System& lua) noexcept;
+            ReaperCore_Lua_System& lua,
+            Event_Manager& events) noexcept;
         void run();
         void request_stop() noexcept;
         void shutdown() noexcept;
@@ -33,10 +36,12 @@ namespace reapercore
         Logging_Manager* m_logging{};
         Settings_System_Manager* m_settings{};
         ReaperCore_Lua_System* m_lua{};
+        Event_Manager* m_events{};
         D3D12_Backend m_d3d12;
         Renderer m_renderer;
         Hook_Registry m_hooks;
         std::atomic_bool m_running{false};
         std::chrono::milliseconds m_tick_interval{50};
+        std::uint64_t m_tick_index{};
     };
 }

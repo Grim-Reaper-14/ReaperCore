50b2eae278210a03ce2f000b421e476101e71027
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 03:56:55 -0400

    Add frontend network page implementation

---
+++ src/frontend/menu/pages/network_page.cpp
@@ -0,0 +1,11 @@
+#include "reapercore/frontend/menu/pages/network_page.hpp"
+
+#include <imgui.h>
+
+namespace reapercore
+{
+    void Network_Page::draw() noexcept
+    {
+        ImGui::TextDisabled("Network page content will be added here.");
+    }
+}

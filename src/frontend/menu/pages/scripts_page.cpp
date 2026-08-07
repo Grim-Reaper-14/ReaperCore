281029c2700b42198754e2ccfa1691e4732481e7
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 05:43:32 -0400

    Add frontend scripts page implementation

---
+++ src/frontend/menu/pages/scripts_page.cpp
@@ -0,0 +1,11 @@
+#include "reapercore/frontend/menu/pages/scripts_page.hpp"
+
+#include <imgui.h>
+
+namespace reapercore
+{
+    void Scripts_Page::draw() noexcept
+    {
+        ImGui::TextDisabled("Scripts page content will be added here.");
+    }
+}

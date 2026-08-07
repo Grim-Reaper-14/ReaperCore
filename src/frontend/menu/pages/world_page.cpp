8b432c560e8181b13c6e68a5c0815152675a9745
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 01:54:09 -0400

    Add frontend world page implementation

---
+++ src/frontend/menu/pages/world_page.cpp
@@ -0,0 +1,11 @@
+#include "reapercore/frontend/menu/pages/world_page.hpp"
+
+#include <imgui.h>
+
+namespace reapercore
+{
+    void World_Page::draw() noexcept
+    {
+        ImGui::TextDisabled("World page content will be added here.");
+    }
+}

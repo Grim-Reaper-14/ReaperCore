2b7178ffc20b8e541c7fb4eb4d7f73378a167348
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 01:34:11 -0400

    Add frontend home page implementation

---
+++ src/frontend/menu/pages/home_page.cpp
@@ -0,0 +1,11 @@
+#include "reapercore/frontend/menu/pages/home_page.hpp"
+
+#include <imgui.h>
+
+namespace reapercore
+{
+    void Home_Page::draw() noexcept
+    {
+        ImGui::TextDisabled("Home page content will be added here.");
+    }
+}

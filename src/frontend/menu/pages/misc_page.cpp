edbebe52f6b815867c8c62a47e6545698262bd05
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 06:40:39 -0400

    Add frontend misc page implementation

---
+++ src/frontend/menu/pages/misc_page.cpp
@@ -0,0 +1,11 @@
+#include "reapercore/frontend/menu/pages/misc_page.hpp"
+
+#include <imgui.h>
+
+namespace reapercore
+{
+    void Misc_Page::draw() noexcept
+    {
+        ImGui::TextDisabled("Misc page content will be added here.");
+    }
+}

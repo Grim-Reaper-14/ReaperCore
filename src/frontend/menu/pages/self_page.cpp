f120d0115e65fcaa2aa758c7f13b0490a2a145fb
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 01:42:44 -0400

    Add frontend self page implementation

---
+++ src/frontend/menu/pages/self_page.cpp
@@ -0,0 +1,11 @@
+#include "reapercore/frontend/menu/pages/self_page.hpp"
+
+#include <imgui.h>
+
+namespace reapercore
+{
+    void Self_Page::draw() noexcept
+    {
+        ImGui::TextDisabled("Self page content will be added here.");
+    }
+}

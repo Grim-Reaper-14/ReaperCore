fc458ff2026938920ef489785aca55d889b4c25f
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 01:46:16 -0400

    Add frontend vehicle page implementation

---
+++ src/frontend/menu/pages/vehicle_page.cpp
@@ -0,0 +1,11 @@
+#include "reapercore/frontend/menu/pages/vehicle_page.hpp"
+
+#include <imgui.h>
+
+namespace reapercore
+{
+    void Vehicle_Page::draw() noexcept
+    {
+        ImGui::TextDisabled("Vehicle page content will be added here.");
+    }
+}
